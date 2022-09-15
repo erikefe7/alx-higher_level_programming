cb402c332ddf8b790f18d09cefac76a5e66a222f
erikefe7 <erikefeugherughe@gmail.com>
2022-09-14 20:16:13 -0700

    updated task 100

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,26 +3,39 @@
 
 
 class Node:
+    """ the node of the sigly linked list
+    """
+
     def __init__(self, data, next_node=None):
+        """ initializing the node
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """ retrieving the property of the node
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """ setting the value of the node
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """ retrieving the private node data
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """ using the retrieved private data to complete the list
+        """
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
@@ -30,6 +43,8 @@ class Node:
 
 class SinglyLinkedList:
     def __str__(self):
+        """ the singly linked list
+        """
         rtn = ""
         ptr = self.__head
 
@@ -42,9 +57,13 @@ class SinglyLinkedList:
         return rtn
 
     def __init__(self):
+        """ the initialization of the value of the head node
+        """
         self.__head = None
 
     def sorted_insert(self, value):
+        """ adding value to the node
+        """
         ptr = self.__head
 
         while ptr is not None:
